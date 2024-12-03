119cfe6af89e9a668f836ef966aa4052cfba6ad8
CMClark1 <caira.clark@dfo-mpo.gc.ca>
2024-12-03 09:03:44 -0400

    documentation

--- R/calc_weight_ab.R
+++ R/calc_weight_ab.R
@@ -1,4 +1,19 @@
-calc_weight_ab <- function(data=NULL, ab_file=spoctacklebox::survey_ab, sex="sex",code="code",season="season"){
+#' Calculate Fish Weights from Survey As and Bs
+#'
+#' @param data A dataframe containing columns for species code, sex, survey season, and length
+#' @param ab_file A dataframe containing the survey a and b values
+#' @param sex Sex code as used on the surveys: 0 (unspecified), 1 (male), or 2 (female)
+#' @param code Species code as used on the surveys: 10 (cod), 11 (haddock), 14 (silver hake), 16 (pollock)
+#' @param season Survey season: "summer" or "winter"
+#' @param flen Fish length, in the same units as the ab_file.
+#'
+#' @return "data" input file with additional columns for calculated weight (weight_ab) and weight units
+#' @export
+#'
+#' @examples
+#' calc_weight_ab(data=surveydata)
+
+calc_weight_ab <- function(data=NULL, ab_file=spoctackle::survey_ab, sex="sex",code="code",season="season", flen="flen"){
   sex_stratified <- data |>
     dplyr::filter(code%in%c(14,220,2550)) |>
     dplyr::left_join(ab_file, by=c(sex, code, season)) |>
