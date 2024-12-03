4abb381890b16936c3a339fdeec7e966505e6565
CMClark1 <caira.clark@dfo-mpo.gc.ca>
2024-12-03 14:40:14 -0400

    join issue

--- R/calc_weight_ab.R
+++ R/calc_weight_ab.R
@@ -1,36 +1,59 @@
 #' Calculate Fish Weights from Survey As and Bs
 #'
-#' @param data A dataframe containing columns for species code, sex, survey season, and length in the format that they are used in the survey database (SPEC,FSEX,SEASON,FLEN)
-#' @param ab_file A dataframe containing the survey a and b values
+#' @param data A dataframe containing columns for species code, sex, survey season, and length in the format that they are used in the survey database (SPEC,FSEX,SEASON,FLEN). To run for As and Bs, by quarter, it also needs to have a numeric "MONTH" column.
+#' @param ab_source Source of the ab values: "survey" (Noble and Clark 2017) or "other" which will default to quartly As and Bs as used in Catch.
 #'
 #' @return "data" input file with additional columns for calculated weight (weight_ab) and weight units
 #' @export
 #'
 
-calc_weight_ab <- function(data = NULL, ab_file = spoctackle::survey_ab) {
-  data$SEASON <- base::tolower(data$SEASON)
-  sex_stratified <- data |>
-    janitor::clean_names() |>
-    dplyr::rename("code" = "spec", "sex" = "fsex") |>
-    dplyr::filter(code %in% c(14, 220, 2550)) |>
-    dplyr::left_join(ab_file, by = c("sex", "code", "season")) |>
-    dplyr::mutate(
-      weight_ab = length_weight_a * (flen^length_weight_b),
-      sex_stratified_ab = TRUE
-    ) |>
-    dplyr::select(-n, -length_weight_a, -length_weight_b, -max_length, -length_units, -r, -common_name) |>
-    dplyr::rename("spec"="code","fsex"="sex")
-  sex_unstratified <- data |>
-    janitor::clean_names() |>
-    dplyr::rename("code" = "spec", "sex" = "fsex") |>
-    dplyr::filter(!code %in% c(14, 220, 2550)) |>
-    dplyr::left_join(ab_file, by = c("code", "season")) |>
-    dplyr::mutate(
-      weight_ab = length_weight_a * (flen^length_weight_b),
-      sex_stratified_ab = FALSE
-    ) |>
-    dplyr::select(-n, -length_weight_a, -length_weight_b, -max_length, -length_units, -r, -common_name, -sex.y)%>%
-    dplyr::rename("spec"="code","fsex"="sex.x")
-  all_weights <- dplyr::full_join(sex_stratified, sex_unstratified)
-  base::print(all_weights)
+calc_weight_ab <- function(data = NULL, ab_source = NULL) {
+
+  if (ab_source == "survey") {
+    ab_file <- spoctackle::survey_ab
+    data$SEASON <- base::tolower(data$SEASON)
+    sex_stratified <- data |>
+      janitor::clean_names() |>
+      dplyr::rename("code" = "spec", "sex" = "fsex") |>
+      dplyr::filter(code %in% c(14, 220, 2550)) |>
+      dplyr::left_join(ab_file, by = c("sex", "code", "season")) |>
+      dplyr::mutate(
+        weight_ab = length_weight_a * (flen^length_weight_b),
+        sex_stratified_ab = TRUE
+      ) |>
+      dplyr::select(-n, -length_weight_a, -length_weight_b, -max_length, -length_units, -r, -common_name) |>
+      dplyr::rename("spec" = "code", "fsex" = "sex")
+    sex_unstratified <- data |>
+      janitor::clean_names() |>
+      dplyr::rename("code" = "spec", "sex" = "fsex") |>
+      dplyr::filter(!code %in% c(14, 220, 2550)) |>
+      dplyr::left_join(ab_file, by = c("code", "season")) |>
+      dplyr::mutate(
+        weight_ab = length_weight_a * (flen^length_weight_b),
+        sex_stratified_ab = FALSE
+      ) |>
+      dplyr::select(-n, -length_weight_a, -length_weight_b, -max_length, -length_units, -r, -common_name, -sex.y) %>%
+      dplyr::rename("spec" = "code", "fsex" = "sex.x")
+    all_weights <- dplyr::full_join(sex_stratified, sex_unstratified)
+    base::print(all_weights)
+
+  } else {
+    ab_file <- spoctackle::commercial_ab
+    cod4X <- data |>
+      janitor::clean_names() |>
+      dplyr::filter(spec == 10) %>%
+      dplyr::mutate(quarter = dplyr::case_when(
+        month %in% 1:3 ~ 1,
+        month %in% 4:6 ~ 2,
+        month %in% 7:9 ~ 3,
+        month %in% 10:12 ~ 4
+      )) %>%
+      dplyr::left_join(ab_file, by = c("quarter")) |>
+      dplyr::mutate(
+        weight_ab = length_weight_a * (flen^length_weight_b),
+        sex_stratified_ab = FALSE
+      ) |>
+      dplyr::select(-stock,-sex,-length_weight_a,-length_weight_b,-code)
+    base::print(cod4X)
+  }
 }
