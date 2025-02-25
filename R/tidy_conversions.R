84293ecee574ba5350844031492998edfe207fb3
CMClark1 <caira.clark@dfo-mpo.gc.ca>
2025-02-25 12:22:33 +0000

    tidied

--- R/tidy_conversions.R
+++ R/tidy_conversions.R
@@ -12,171 +12,185 @@
 #'
 #'
 
-tidy_conversions <- function(login=NULL, pw=NULL, dsn=NULL, species_list=10, season="SUMMER", localdir=NULL){
+tidy_conversions <- function(login = NULL, pw = NULL, dsn = NULL, species_list = 10, season = "SUMMER", localdir = NULL) {
+  if (stringr::str_to_upper(season) == stringr::str_to_upper("summer")) {
+    channel <- ROracle::dbConnect(DBI::dbDriver("Oracle"), username = login, password = pw, dsn)
 
-if(stringr::str_to_upper(season)==stringr::str_to_upper("summer")){
+    gsconversions <- ROracle::dbGetQuery(channel, paste("select * from groundfish.gsconversions")) |> janitor::clean_names()
 
-  channel <- ROracle::dbConnect(DBI::dbDriver("Oracle"), username=login, password=pw, dsn)
+    conversion <- gsconversions |>
+      dplyr::filter(spec %in% species_list & season == season)
 
-  gsconversions <- ROracle::dbGetQuery(channel, paste("select * from groundfish.gsconversions")) |> janitor::clean_names()
+    conversion_nedtel <- conversion |>
+      dplyr::filter(from_vessel == "NED_TEM" & to_vessel == "TEL_VEN") |>
+      dplyr::select(spec, flen, cf_value) %>%
+      dplyr::rename("cf_value_ned_to_tel" = "cf_value")
 
-  conversion <- gsconversions |>
-    dplyr::filter(spec %in% species_list & season==season)
+    conversion_telcar <- conversion |>
+      dplyr::filter(from_vessel == "TEL_VEN" & to_vessel == "CAR_CAB") |>
+      dplyr::select(spec, flen, cf_value) %>%
+      dplyr::rename("cf_value_tel_to_car" = "cf_value")
 
-  conversion_nedtel <- conversion |>
-    dplyr::filter(from_vessel=="NED_TEM" & to_vessel == "TEL_VEN") |>
-    dplyr::select(spec,flen,cf_value) %>%
-    dplyr::rename("cf_value_ned_to_tel"="cf_value")
+    conversion_atcham <- base::data.frame(spec = c(11, 40, 41, 42, 43), cf_value_atcham_to_ned = c(1.2, 0.8, 0.8, 0.8, 0.8))
 
-  conversion_telcar <- conversion |>
-    dplyr::filter(from_vessel=="TEL_VEN" & to_vessel == "CAR_CAB") |>
-    dplyr::select(spec,flen,cf_value) %>%
-    dplyr::rename("cf_value_tel_to_car"="cf_value")
+    # Needler equivalent
 
-  conversion_atcham <- base::data.frame(spec=c(11,40,41,42,43),cf_value_atcham_to_ned=c(1.2,0.8,0.8,0.8,0.8))
+    # ATC/Hammond to Needler
 
-  #Needler equivalent
+    atcham_needler <- conversion_nedtel |>
+      dplyr::bind_rows(conversion_nedtel) |>
+      dplyr::mutate(vesel = base::rep(c("A", "H"), each = base::nrow(conversion_nedtel))) |>
+      dplyr::select(-cf_value_ned_to_tel) |>
+      dplyr::left_join(conversion_atcham, relationship = "many-to-many")
 
-  #ATC/Hammond to Needler
+    # Teleost/Venturer to Needler
+    teleost_needler <- conversion_nedtel |>
+      dplyr::bind_rows(conversion_nedtel) |>
+      dplyr::mutate(
+        cf_value_tel_to_ned = 1 / cf_value_ned_to_tel,
+        vesel = base::rep(c("S", "V"), each = base::nrow(conversion_nedtel))
+      ) |>
+      dplyr::select(-cf_value_ned_to_tel)
 
-  atcham_needler <- conversion_nedtel |>
-    dplyr::bind_rows(conversion_nedtel) |>
-    dplyr::mutate(vesel=base::rep(c("A","H"),each=base::nrow(conversion_nedtel))) |>
-    dplyr::select(-cf_value_ned_to_tel) |>
-    dplyr::left_join(conversion_atcham, relationship="many-to-many")
+    # Cartier to Needler
+    cartier_needler <- conversion_telcar |>
+      dplyr::left_join(conversion_nedtel) |>
+      dplyr::bind_rows(conversion_telcar |> dplyr::left_join(conversion_nedtel)) |>
+      dplyr::mutate(
+        cf_value_car_to_tel = 1 / cf_value_tel_to_car,
+        cf_value_tel_to_ned = 1 / cf_value_ned_to_tel,
+        vesel = base::rep(c("J", "B"), each = base::nrow(conversion_telcar))
+      ) |>
+      dplyr::select(-cf_value_tel_to_car, -cf_value_ned_to_tel)
 
-  #Teleost/Venturer to Needler
-  teleost_needler <- conversion_nedtel |>
-    dplyr::bind_rows(conversion_nedtel) |>
-    dplyr::mutate(cf_value_tel_to_ned=1/cf_value_ned_to_tel,
-           vesel=base::rep(c("S","V"),each=base::nrow(conversion_nedtel))) |>
-    dplyr::select(-cf_value_ned_to_tel)
+    joined_needler <- plyr::join_all(base::list(atcham_needler, teleost_needler, cartier_needler), by = c("vesel", "spec", "flen"), type = "full") %>%
+      base::replace(base::is.na(.), 1) |>
+      dplyr::mutate(
+        conversion = 1 / (cf_value_atcham_to_ned * cf_value_tel_to_ned * cf_value_car_to_tel),
+        vesseleq = "needler"
+      )
 
-  #Cartier to Needler
-  cartier_needler <- conversion_telcar |>
-    dplyr::left_join(conversion_nedtel) |>
-    dplyr::bind_rows(conversion_telcar |> dplyr::left_join(conversion_nedtel)) |>
-    dplyr::mutate(cf_value_car_to_tel=1/cf_value_tel_to_car,
-           cf_value_tel_to_ned=1/cf_value_ned_to_tel,
-           vesel=base::rep(c("J","B"),each=base::nrow(conversion_telcar))) |>
-    dplyr::select(-cf_value_tel_to_car,-cf_value_ned_to_tel)
+    # Teleost equivalent
 
-  joined_needler <- plyr::join_all(base::list(atcham_needler,teleost_needler,cartier_needler), by=c("vesel","spec","flen"), type='full') %>%
-    base::replace(base::is.na(.), 1) |>
-    dplyr::mutate(conversion=1/(cf_value_atcham_to_ned*cf_value_tel_to_ned*cf_value_car_to_tel),
-           vesseleq="needler")
+    # ATC/Hammond to Needler to Teleost
 
-  #Teleost equivalent
+    atcham_teleost <- conversion_nedtel |>
+      dplyr::bind_rows(conversion_nedtel) |>
+      dplyr::mutate(vesel = base::rep(c("A", "H"), each = base::nrow(conversion_nedtel))) |>
+      dplyr::left_join(conversion_atcham, relationship = "many-to-many")
 
-  #ATC/Hammond to Needler to Teleost
+    # Needler/Templeman to Teleost
+    needler_teleost <- conversion_nedtel |>
+      dplyr::bind_rows(conversion_nedtel) |>
+      dplyr::mutate(vesel = base::rep(c("N", "T"), each = base::nrow(conversion_nedtel)))
 
-  atcham_teleost <- conversion_nedtel |>
-    dplyr::bind_rows(conversion_nedtel) |>
-    dplyr::mutate(vesel=base::rep(c("A","H"),each=base::nrow(conversion_nedtel))) |>
-    dplyr::left_join(conversion_atcham, relationship="many-to-many")
+    # Cartier/Cabot to Teleost/Venturer
+    cartier_teleost <- conversion_telcar |>
+      dplyr::bind_rows(conversion_telcar) |>
+      dplyr::mutate(
+        cf_value_car_to_tel = 1 / cf_value_tel_to_car,
+        vesel = base::rep(c("J", "B"), each = base::nrow(conversion_telcar))
+      ) |>
+      dplyr::select(-cf_value_tel_to_car)
 
-  #Needler/Templeman to Teleost
-  needler_teleost <- conversion_nedtel |>
-  dplyr::bind_rows(conversion_nedtel) |>
-    dplyr::mutate(vesel=base::rep(c("N","T"),each=base::nrow(conversion_nedtel)))
+    joined_teleost <- plyr::join_all(list(atcham_teleost, needler_teleost, cartier_teleost), by = c("vesel", "spec", "flen"), type = "full") %>%
+      base::replace(is.na(.), 1) |>
+      dplyr::mutate(
+        conversion = 1 / (cf_value_atcham_to_ned * cf_value_ned_to_tel * cf_value_car_to_tel),
+        vesseleq = "teleost"
+      )
 
-  #Cartier/Cabot to Teleost/Venturer
-  cartier_teleost <- conversion_telcar |>
-    dplyr::bind_rows(conversion_telcar) |>
-    dplyr::mutate(cf_value_car_to_tel=1/cf_value_tel_to_car,
-           vesel=base::rep(c("J","B"),each=base::nrow(conversion_telcar))) |>
-    dplyr::select(-cf_value_tel_to_car)
+    # Cartier equivalent
 
-  joined_teleost <- plyr::join_all(list(atcham_teleost,needler_teleost,cartier_teleost), by=c("vesel","spec","flen"), type='full') %>%
-    base::replace(is.na(.), 1) |>
-    dplyr::mutate(conversion=1/(cf_value_atcham_to_ned*cf_value_ned_to_tel*cf_value_car_to_tel),
-           vesseleq="teleost")
+    # ATC/Hammond to Needler to Teleost to Cartier
 
-  #Cartier equivalent
+    atcham_cartier <- conversion_nedtel |>
+      dplyr::left_join(conversion_telcar) |>
+      dplyr::bind_rows(conversion_nedtel |>
+        dplyr::left_join(conversion_telcar)) |>
+      dplyr::mutate(vesel = base::rep(c("A", "H"), each = base::nrow(conversion_nedtel))) |>
+      dplyr::left_join(conversion_atcham, relationship = "many-to-many")
 
-  #ATC/Hammond to Needler to Teleost to Cartier
+    # Needler/Templeman to Teleost to Cartier
+    needler_cartier <- conversion_nedtel |>
+      dplyr::left_join(conversion_telcar) |>
+      dplyr::bind_rows(conversion_nedtel |>
+        dplyr::left_join(conversion_telcar)) |>
+      dplyr::mutate(vesel = base::rep(c("N", "T"), each = base::nrow(conversion_nedtel)))
 
-  atcham_cartier <- conversion_nedtel |>
-    dplyr::left_join(conversion_telcar) |>
-    dplyr::bind_rows(conversion_nedtel |>
-                dplyr::left_join(conversion_telcar)) |>
-    dplyr::mutate(vesel=base::rep(c("A","H"),each=base::nrow(conversion_nedtel))) |>
-    dplyr::left_join(conversion_atcham, relationship="many-to-many")
+    # Teleost/Venturer to Cartier
+    teleost_cartier <- conversion_telcar |>
+      dplyr::bind_rows(conversion_telcar) |>
+      dplyr::mutate(vesel = base::rep(c("S", "V"), each = base::nrow(conversion_telcar)))
 
-  #Needler/Templeman to Teleost to Cartier
-  needler_cartier <- conversion_nedtel |>
-    dplyr::left_join(conversion_telcar) |>
-    dplyr::bind_rows(conversion_nedtel |>
-                dplyr::left_join(conversion_telcar)) |>
-    dplyr::mutate(vesel=base::rep(c("N","T"),each=base::nrow(conversion_nedtel)))
+    joined_cartier <- plyr::join_all(list(atcham_cartier, needler_cartier, teleost_cartier), by = c("vesel", "spec", "flen"), type = "full") %>%
+      base::replace(is.na(.), 1) |>
+      dplyr::mutate(
+        conversion = 1 / (cf_value_atcham_to_ned * cf_value_ned_to_tel * cf_value_tel_to_car),
+        vesseleq = "cartier"
+      )
 
-  #Teleost/Venturer to Cartier
-  teleost_cartier <- conversion_telcar |>
-    dplyr::bind_rows(conversion_telcar) |>
-    dplyr::mutate(vesel=base::rep(c("S","V"),each=base::nrow(conversion_telcar)))
+    joined_all <- base::rbind(
+      joined_needler |> dplyr::select(spec, flen, vesel, conversion, vesseleq),
+      joined_teleost |> dplyr::select(spec, flen, vesel, conversion, vesseleq),
+      joined_cartier |> dplyr::select(spec, flen, vesel, conversion, vesseleq)
+    )
 
-  joined_cartier <- plyr::join_all(list(atcham_cartier,needler_cartier,teleost_cartier), by=c("vesel","spec","flen"), type='full') %>%
-    base::replace(is.na(.), 1) |>
-    dplyr::mutate(conversion=1/(cf_value_atcham_to_ned*cf_value_ned_to_tel*cf_value_tel_to_car),
-           vesseleq="cartier")
+    base::return(joined_all)
+  } else if (stringr::str_to_upper(season) %in% stringr::str_to_upper("spring", "winter")) {
+    # Add spring conversions for cod and haddock - temporary until these are in the survey database
 
-  joined_all <- base::rbind(joined_needler |> dplyr::select(spec,flen,vesel,conversion,vesseleq),
-                      joined_teleost |> dplyr::select(spec,flen,vesel,conversion,vesseleq),
-                      joined_cartier |> dplyr::select(spec,flen,vesel,conversion,vesseleq))
+    base::load(base::paste(localdir, "//result_conversion.RData", sep = ""))
 
-  base::return(joined_all)
+    result_format <- result_conversion |>
+      dplyr::select(spec, lenseq, est_rho) |>
+      dplyr::rename("flen" = lenseq, "cf_value" = est_rho)
 
-} else if (stringr::str_to_upper(season)%in%stringr::str_to_upper("spring","winter")){
+    maxmins <- base::rbind(
+      base::data.frame(spec = c(base::rep(10, 12), base::rep(11, 18)), flen = c(1:12, 1:18)) |> dplyr::mutate(cf_value = dplyr::case_when(spec == 10 ~ 0.220355888258648, spec == 11 ~ 0.250406234496239)),
+      base::data.frame(spec = c(base::rep(10, 169 - 93), base::rep(11, 90 - 62)), flen = c(94:169, 63:90)) |> dplyr::mutate(cf_value = case_when(spec == 10 ~ 1.284894, spec == 11 ~ 1.695796))
+    )
 
-  #Add spring conversions for cod and haddock - temporary until these are in the survey database
+    spring_conversions <- base::rbind(result_format, maxmins) |> dplyr::mutate(from_vessel = "TEL_VEN", to_vessel = "CAR_CAB", season = "SPRING", cf_model_type = "LDM", cf_metric = "ABUNDANCE", cf_unit = "CM")
 
-  base::load(base::paste(localdir,"//result_conversion.RData",sep=""))
+    conversion <- spring_conversions |>
+      dplyr::filter(spec %in% species_list & season == season)
 
-  result_format <- result_conversion |>
-    dplyr::select(spec,lenseq,est_rho) |>
-    dplyr::rename("flen"=lenseq,"cf_value"=est_rho)
+    conversion_telcar <- conversion |>
+      dplyr::filter(from_vessel == "TEL_VEN" & to_vessel == "CAR_CAB") |>
+      dplyr::select(spec, flen, cf_value) %>%
+      dplyr::rename("cf_value_tel_to_car" = "cf_value")
 
-  maxmins <- base::rbind(base::data.frame(spec=c(base::rep(10,12),base::rep(11,18)),flen=c(1:12,1:18)) |> dplyr::mutate(cf_value=dplyr::case_when(spec==10~0.220355888258648,spec==11~0.250406234496239)),
-                   base::data.frame(spec=c(base::rep(10,169-93),base::rep(11,90-62)),flen=c(94:169,63:90)) |> dplyr::mutate(cf_value=case_when(spec==10~1.284894,spec==11~1.695796)))
+    # Needler/Templeman/Teleost/Venturer to Cartier/Cabot
 
-  spring_conversions <- base::rbind(result_format,maxmins) |> dplyr::mutate(from_vessel="TEL_VEN",to_vessel="CAR_CAB",season="SPRING",cf_model_type="LDM",cf_metric="ABUNDANCE", cf_unit="CM")
+    teleost_cartier <- conversion_telcar |>
+      dplyr::bind_rows(conversion_telcar, conversion_telcar, conversion_telcar) |>
+      dplyr::mutate(vesel = base::rep(c("N", "T", "S", "V"), each = base::nrow(conversion_telcar))) |>
+      dplyr::mutate(
+        conversion = 1 / (cf_value_tel_to_car),
+        vesseleq = "cartier"
+      )
 
-  conversion <- spring_conversions |>
-    dplyr::filter(spec %in% species_list & season==season)
+    # Cartier/Cabot to Needler/Templeman/Teleost/Venturer
 
-  conversion_telcar <- conversion |>
-    dplyr::filter(from_vessel=="TEL_VEN" & to_vessel == "CAR_CAB") |>
-    dplyr::select(spec,flen,cf_value) %>%
-    dplyr::rename("cf_value_tel_to_car"="cf_value")
+    cartier_teleost <- conversion_telcar |>
+      dplyr::bind_rows(conversion_telcar, conversion_telcar, conversion_telcar) |>
+      dplyr::mutate(
+        cf_value_car_to_tel = 1 / cf_value_tel_to_car,
+        vesel = base::rep(c("N", "T", "J", "B"), each = base::nrow(conversion_telcar))
+      ) |>
+      dplyr::select(-cf_value_tel_to_car) |>
+      dplyr::mutate(
+        conversion = 1 / (cf_value_car_to_tel),
+        vesseleq = "teleost"
+      )
 
-  #Needler/Templeman/Teleost/Venturer to Cartier/Cabot
+    # Join em up
 
-  teleost_cartier <- conversion_telcar |>
-    dplyr::bind_rows(conversion_telcar,conversion_telcar,conversion_telcar) |>
-    dplyr::mutate(vesel=base::rep(c("N","T","S","V"), each=base::nrow(conversion_telcar))) |>
-    dplyr::mutate(conversion=1/(cf_value_tel_to_car),
-           vesseleq="cartier")
+    joined_all <- base::rbind(teleost_cartier |> dplyr::select(spec, flen, vesel, conversion, vesseleq), cartier_teleost |> dplyr::select(spec, flen, vesel, conversion, vesseleq))
 
-  #Cartier/Cabot to Needler/Templeman/Teleost/Venturer
-
-  cartier_teleost <- conversion_telcar |>
-    dplyr::bind_rows(conversion_telcar,conversion_telcar,conversion_telcar) |>
-    dplyr::mutate(cf_value_car_to_tel=1/cf_value_tel_to_car,
-           vesel=base::rep(c("N","T","J","B"), each=base::nrow(conversion_telcar))) |>
-    dplyr::select(-cf_value_tel_to_car) |>
-    dplyr::mutate(conversion=1/(cf_value_car_to_tel),
-           vesseleq="teleost")
-
-  #Join em up
-
-  joined_all <- base::rbind(teleost_cartier |> dplyr::select(spec,flen,vesel,conversion,vesseleq),cartier_teleost |> dplyr::select(spec,flen,vesel,conversion,vesseleq))
-
-  base::return(joined_all)
-
-} else {
-
-  base::return(print("Season not summer, spring, or winter."))
-
-}
-
+    base::return(joined_all)
+  } else {
+    base::return(print("Season not summer, spring, or winter."))
+  }
 }
